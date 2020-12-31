179418ecd7417dea8922dc315c62fa6557a9b84e
parkjungkwan <pakjkwan@gmail.com>
2020-12-31 17:12:53 +0900

    17

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,9 +1,12 @@
 package hello;
 
 import java.util.Arrays;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Collectors;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        Arrays.asList("Hello, World!!!!").stream().forEach(System.out::print);
+    public String main() {
+        return Arrays.asList(new String[] { "Hello, ", "World ", "!!!!" }).stream().map(i -> String.valueOf(i))
+                .collect(Collectors.joining());
     }
 }
